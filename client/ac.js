27ee114c2bf18a1d993244a894d4c9f19c099c88
Stoku <thestoku@gmail.com>
2024-10-12 13:55:14 +0200

    Add simple anticheat system.

---
+++ client/ac.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Simple anticheat.
+// TODO: Add locales.
+
+function suspectedTrainerUsage() {
+    message('Suspected trainer usage. Leaving session...');
+    triggerNetworkEvent('gui.disconnect');
+}
+
+function healthCheck() {
+    if (localPlayer.health > 251) {
+        suspectedTrainerUsage();
+    }
+}
+
+function armourCheck() {
+    if (localPlayer.armour > 251) {
+        suspectedTrainerUsage();
+    }
+}
+
+function weaponCheck() {
+    // Grenade and Molotov aren't used on the server at all cause of desync. M16 and RL are also unavailable on the "open map", so check for dojoid.
+    const dojoId = localClient.getData('dojo');
+    
+    if (dojoId == null) {
+        if (localPlayer.weapon == WEAPON_M16 ||
+            localPlayer.weapon == WEAPON_ROCKETLAUNCHER ||
+            localPlayer.weapon == WEAPON_MOLOTOV ||
+            localPlayer.weapon == WEAPON_GRENADE) {
+                suspectedTrainerUsage();
+        }
+    }
+}
+
+addEventHandler('OnEntityProcess', function(event, entity) {
+	if (localPlayer != null && isConnected) {
+		if (entity == localPlayer) {
+			healthCheck();
+            armourCheck();
+            weaponCheck();
+		}
+	}
+});
