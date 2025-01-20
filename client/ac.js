aeee2e61cb7802e475d4e376347439909b127868
Stoku <thestoku@gmail.com>
2025-01-20 19:45:04 +0100

    Add check for game ID in weaponCheck

--- client/ac.js
+++ client/ac.js
@@ -24,7 +24,7 @@ function weaponCheck() {
     // Grenade and Molotov aren't used on the server at all cause of desync. M16 and RL are also unavailable on the "open map", so check for dojoid.
     const dojoId = localClient.getData('dojo');
 
-    if (dojoId == null) {
+    if (dojoId == null && gta.game == GAME_GTA_III) {
         if (localPlayer.weapon == WEAPON_M16 ||
             localPlayer.weapon == WEAPON_ROCKETLAUNCHER ||
             localPlayer.weapon == WEAPON_MOLOTOV ||
