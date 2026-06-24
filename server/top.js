5abbb1603420a7933add33bfd5c6c62a242b204f
Stoku <thestoku@gmail.com>
2026-06-24 19:58:14 +0200

    Fix 'top' logic

--- server/top.js
+++ server/top.js
@@ -4,8 +4,8 @@
     Top stats system, it is a cancer, and it's a bit bugged. Needs refactoring.
 */
 
-const TOP_DEFINES = ['kills', 'deaths', 'suicides', 'topSpree', 'sentMessages', 'joins', 'jackedVehicles', 'xp', 'dodoFlightTime', 'mileage', 'onlineTime'];
-const TOP_COMMAND_DEFINES = ['kills', 'deaths', 'suicides', 'spree', 'messages', 'joins', 'jacks', 'xp', 'dodo', 'mileage', 'online'];
+const TOP_DEFINES = ['kills', 'deaths', 'suicides', 'topSpree', 'sentMessages', 'joins', 'jackedVehicles', 'xp', 'dodoFlightTime', 'mileage', 'onlineTime', 'convoys', 'hiddenPackages', "races"];
+const TOP_COMMAND_DEFINES = ['kills', 'deaths', 'suicides', 'spree', 'messages', 'joins', 'jacks', 'xp', 'dodo', 'mileage', 'online', 'convoys', "packages", "races"];
 const TOP_UNITS = [null, null, null, null, null, null, null, null, 's', 'km', 'min'];
 
 const topDB = {
@@ -20,6 +20,9 @@ const topDB = {
 	'dodoFlightTime': [],
 	'mileage': [],
 	'onlineTime': [],
+	'convoys': [],
+	'hiddenPackages': [],
+	'races': [],
 };
 
 function loadTopScores() {
@@ -43,33 +46,37 @@ function printTop(client, key) {
 
 function generateTop(client, index) {
 	const key = TOP_DEFINES[index];
-	const top = [];
 	const unit = TOP_UNITS[index] != null ? TOP_UNITS[index] : '';
 
-	for (let i = 0; i < topDB[key].length; i++) {
-		let buffer = topDB[key][i];
+	const combinedStats = new Map();
 
-		// Create top of current game stats
-		Players.find((element) => {
-			if (typeof element != 'undefined' && element != null) {
-				if (element.db.id === topDB[key][i].id) {
-					console.log(`[TOP] matched: ${topDB[key][i].id} / ${element.db.id}`);
-
-					topDB[key][i][key] = element.db[key];
-
-					buffer = topDB[key][i];
-				}
-			}
+	if (topDB[key]) {
+		topDB[key].forEach(player => {
+			combinedStats.set(player.id, { ...player });
 		});
-
-		top.push(buffer);
 	}
+
+	Players.forEach((element) => {
+		if (typeof element != 'undefined' && element != null && element.db && element.db.id) {
+			combinedStats.set(element.db.id, {
+				id: element.db.id,
+				name: element.name,
+				[key]: element.db[key] || 0
+			});
+		}
+	});
+
+	const topArray = Array.from(combinedStats.values());
+
+	topArray.sort((a, b) => b[key] - a[key]);
+
+	const limit = 10;
+	const top = topArray.slice(0, limit);
 
 	Locale.sendMessage(client, false, COLOUR_ORANGE, 'topMessage1', TOP_COMMAND_DEFINES[index]);
 
-	top.forEach((element, index) => {
-		const message = `${index+1}. ${element.name}: ${element[key]}${unit}`;
-
+	top.forEach((element, idx) => {
+		const message = `${idx + 1}. ${element.name}: ${element[key]}${unit}`;
 		Locale.sendMessage(client, false, COLOUR_YELLOW, 'topMessage2', message);
 	});
 }
